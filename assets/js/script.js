ed058a110192f182bc61e15d24a0cbbfe15c60d6
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2019-11-23 09:13:31 -0500

    appended information from ajax call for currentResultsDiv

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,23 @@ $(document).ready(function () {
     // =========================VARIBLES===========================
     var APIkey = "1fb03cd6dac17b324934023f63ff9654";
     var cities = [];
+    var curretResultsDiv = $("#current-weather-div");
     // =========================FUNCTIONS===========================
 
     function displayCurrentWeather(){
         //======Varibles======
         var cityNameDiv = $("<h3>");
-        var temp = $("<div>");
-        var humidity = $("<div>");
-        var windSpeed = $("<div>");
+        var iconImg = $("<img>")
+        var tempDiv = $("<div>");
+        var humidityDiv = $("<div>");
+        var windSpeedDiv = $("<div>");
         
         var cityCurrent = $(this).attr("data-city");
-        console.log("this was clicked: " + cityCurrent);
+        console.log("this was city's weather was requested: " + cityCurrent);
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityCurrent + "&units=imperial&appid=" + APIkey;
+
+        // Clear currentResultsDiv
+        curretResultsDiv.empty();
 
         // Ajax call
         $.ajax({
@@ -21,11 +26,43 @@ $(document).ready(function () {
             method: "GET"
         })
 
-        .then(function(response){
-            console.log(response);
+        .then(function(a){
+            console.log(a);
+
+            // Store city name 
+            var cityName = a.name;
+            cityNameDiv.text(cityName);
+            curretResultsDiv.append(cityNameDiv);
+
+            // Display Icon
+            var iconPull = a.weather[0].icon; 
+            console.log(iconPull);
+            iconImg.attr("src", "http://openweathermap.org/img/w/" + iconPull + ".png");
+            cityNameDiv.append(iconImg);
+
+            // Store temperature
+            var temp = a.main.temp;
+            var tempRounded = temp.toFixed(1);
+            tempDiv.text("Temperature: " + tempRounded + " °F");
+            curretResultsDiv.append(tempDiv);
+
+             // Store humidity
+             var humidity = a.main.humidity;
+             humidityDiv.text("Humidity: " + humidity + "%");
+             curretResultsDiv.append(humidityDiv);
+
+            //  Store wind speed
+            var windSpeed = a.wind.speed;
+            var windRounded = windSpeed.toFixed(1);
+            windSpeedDiv.text("Wind Speed: " + windRounded + " MPH");
+            curretResultsDiv.append(windSpeedDiv);
         })
     }
     
+    function UVIndexFunc(){
+        
+    }
+
     function startup() {
         // Array that holds strings from localStorage
         var last = JSON.parse(localStorage.getItem("City"));
@@ -84,6 +121,7 @@ $(document).ready(function () {
         // Save button text to localStorage
         localStorage.setItem("City", JSON.stringify(cities));
         renderButtons();
+        // something to reset userinput box
     });
 
 
