802ccb6393bfc036a36baa95dfa111668683ca3b
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2019-11-26 10:12:25 -0500

    edited startup function so that the last searched result appends when page loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function (a) { 
+        }).then(function (a) {
             console.log(a); //console response from ajax
 
             // Store city name 
@@ -255,6 +255,222 @@ $(document).ready(function () {
             $(newButtonDiv).append(resultButton);
             $("#city-buttons-lastdiv").prepend(newButtonDiv);
         }
+
+        //======Varibles======
+        var cityNameDiv = $("<h3>");
+        var iconImg = $("<img>")
+        var tempDiv = $("<div>");
+        var humidityDiv = $("<div>");
+        var windSpeedDiv = $("<div>");
+
+        var lastCity = last[last.length - 1];
+        console.log("this was last city's weather that was requested: " + lastCity);
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + lastCity + "&units=imperial&appid=" + APIkey;
+
+        // Clear currentResultsDiv
+        curretResultsDiv.empty();
+
+        // Ajax call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (a) {
+            console.log(a); //console response from ajax
+
+            // Store city name 
+            var cityName = a.name;
+            cityNameDiv.text(cityName + " (" + moment().format('l') + ")");
+            curretResultsDiv.append(cityNameDiv);
+
+            // Display Icon
+            var iconPull = a.weather[0].icon;
+            console.log(iconPull);
+            iconImg.attr("src", "http://openweathermap.org/img/w/" + iconPull + ".png");
+            cityNameDiv.append(iconImg);
+
+            // Store temperature
+            var temp = a.main.temp;
+            var tempRounded = temp.toFixed(1);
+            tempDiv.text("Temperature: " + tempRounded + " °F");
+            curretResultsDiv.append(tempDiv);
+
+            // Store humidity
+            var humidity = a.main.humidity;
+            humidityDiv.text("Humidity: " + humidity + "%");
+            curretResultsDiv.append(humidityDiv);
+
+            //  Store wind speed
+            var windSpeed = a.wind.speed;
+            var windRounded = windSpeed.toFixed(1);
+            windSpeedDiv.text("Wind Speed: " + windRounded + " MPH");
+            curretResultsDiv.append(windSpeedDiv);
+
+            // function to make ajax call for UVindex
+            function UVIndexFunc() {
+                // store lattitude from previous ajax call
+                var lat = a.coord.lat;
+                console.log("This is the latitiude: " + lat);
+
+                // store lattitude from previous ajax call
+                var long = a.coord.lon;
+                console.log("This is the longitiude: " + long);
+
+                // =======Varibles=======
+                var uvIndexDiv = $("<div>");
+                var badgeSpan = $("<span>");
+                badgeSpan.addClass("badge badge-danger");
+                // badgeSpan.parent().attr("id", "bagdeParent");
+                // badgeSpan.parent().css("display", "inline-block")
+
+                var queryURLUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=" + APIkey;
+
+                // Ajax call
+                $.ajax({
+                    url: queryURLUV,
+                    method: "GET"
+                }).then(function (uvResponse) {
+                    console.log(uvResponse);
+
+                    // Display UV information
+                    var uvResult = uvResponse.value;
+                    badgeSpan.text(uvResult);
+                    uvIndexDiv.append(badgeSpan);
+                    curretResultsDiv.append("UV Index: ", uvIndexDiv);
+                    // curretResultsDiv.append(uvIndexDiv);
+                })
+
+            }
+            UVIndexFunc();
+        })
+        // Clear fiveDayDiv
+        fiveDayDiv.empty();
+
+        // =======Varibles=======
+        var lastCityFive = last[last.length - 1];
+        console.log("this was last city's weather that was requested: " + lastCityFive);
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + lastCityFive + "&units=imperial&appid=" + APIkey;
+        var m = 0;
+        var n = 0;
+        var o = 4;
+        var p = 0;
+
+        // Ajax call
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (a) {
+            console.log(a);
+
+            // loop through each day
+            for (var i = 0; i < 40; i += 8) {
+                console.log("i = " + i);
+                n += 8;
+                console.log("n = " + n);
+                p++;
+                console.log("p = " + p);
+
+
+                // =======Varibles=======
+                var dateH5 = $("<h5>");
+                var iconImg = $("<img>");
+                var b = a.list[o];
+
+                var miniWeatherDiv = $("<div>");
+                miniWeatherDiv.addClass("card");
+
+                var miniWeatherBody = $("<div>");
+                miniWeatherBody.addClass("card-body");
+
+                // Display date
+                var date = moment().add(p, 'days').calendar('L');
+                console.log("Five day forcast date is: " + date);
+                dateH5.text(date);
+                miniWeatherBody.append(dateH5);
+
+
+                // Display icon
+                var iconPull = b.weather[0].icon;
+                console.log("o = " + o);
+                console.log(iconPull);
+                iconImg.attr("src", "http://openweathermap.org/img/w/" + iconPull + ".png");
+                miniWeatherBody.append(iconImg);
+
+                o += 8;
+                console.log("o = " + o);
+
+                // Store temperature and humidity for every 3 hours in day
+                var tempArray = [];
+                var humArray = [];
+
+                // variables to store totals
+                var sumTemp = 0;
+                var sumHum = 0;
+
+                // for loop to generate five day weather
+                for (m; m < n; m++) {
+
+                    // Hold 3 hour temperature information in array
+                    var thisTemp = a.list[m].main.temp;
+                    tempArray.push(thisTemp);
+                    console.log("Here is the tempArray: " + tempArray);
+
+                    // Hold 3 hour humidity information in array
+                    var thisHum = a.list[m].main.humidity;
+                    humArray.push(thisHum);
+                    console.log("Here is the humArray: " + humArray);
+
+                    console.log("This is maybe m: " + m);
+                    // if statement to collect information for every day inorder to get average
+                    if (m === 7 || m === 15 || m === 23 || m === 31 || m === 39) {
+                        // empty div
+                        miniWeatherDiv.empty();
+                        var tempFiveDiv = $("<div>");
+                        var humidityFiveDiv = $("<div>");
+
+                        console.log("I'm in the if statement!!!");
+                        // get total of humidity and temperature
+                        for (var k = 0; k < 8; k++) {
+                            sumTemp += tempArray[k];
+                            console.log("SumTemp = " + sumTemp);
+                            sumHum += humArray[k];
+                            console.log("SumHum = " + sumHum);
+                        }
+
+                        // get and store average of temperature
+                        var tempAvg = sumTemp / tempArray.length;
+                        console.log("tempAvg = " + tempAvg);
+                        // round to 1 decimal
+                        var tempRounded = tempAvg.toFixed(1);
+
+                        // get and store average of humdity
+                        var humAvg = sumHum / humArray.length;
+                        console.log("humAvg = " + humAvg);
+                        // round to 1 decimal
+                        var humidityRounded = humAvg.toFixed(1);
+
+                        // append information to page
+                        tempFiveDiv.text("Temperature: " + tempRounded + " °F");
+                        humidityFiveDiv.text("Humidity: " + humidityRounded + "%");
+                        miniWeatherBody.append(tempFiveDiv);
+                        miniWeatherBody.append(humidityFiveDiv);
+                        console.log("This is inside miniWeatherBody: " + JSON.stringify(miniWeatherBody));
+                        miniWeatherDiv.append(miniWeatherBody);
+                        console.log("This is inside miniWeatherDiv: " + JSON.stringify(miniWeatherDiv));
+                        fiveDayDiv.append(miniWeatherDiv);
+
+                        // Reset Arrays
+                        tempArray = [];
+                        humArray = [];
+                        // reset variables for
+                        sumTemp = 0;
+                        sumHum = 0;
+
+                        console.log("Leaving the IF statement");
+                    }
+                }
+            }
+        })
     }
 
 
